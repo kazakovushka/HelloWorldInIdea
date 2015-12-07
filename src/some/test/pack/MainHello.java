3a238301f7361ffc74a27e04b52f00085a549ff3
kazakovushka <kazakovushka@gmail.com>
2015-12-07 13:40:23 +0300

    just hello world class add

---
+++ src/some/test/pack/MainHello.java
@@ -0,0 +1,12 @@
+package some.test.pack;
+
+/**
+ * Created by kazakova on 02.12.15.
+ */
+public class MainHello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+
+}
---
+++ src/some/test/pack/MainHelloTest.java
@@ -0,0 +1,19 @@
+package some.test.pack;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by kazakova on 02.12.15.
+ */
+public class MainHelloTest {
+
+    @Test
+    public void someTest(){
+
+        System.out.println("testyy");
+        Assert.assertTrue(true);
+    }
+}
